e7c610d04e1da48520e06f4776c5e01efe409f87
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-09-07 09:14:41 +0100

    reformatted utc string

--- HNGx Stage 1/controllers/apiController.js
+++ HNGx Stage 1/controllers/apiController.js
@@ -5,11 +5,10 @@ const getCurrentUTCTime = function () {
   const month = String(now.getUTCMonth() + 1).padStart(2, '0');
   const day = String(now.getUTCDate()).padStart(2, '0');
   const hours = String(now.getUTCHours()).padStart(2, '0');
+  const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
-  // Round the minutes to the nearest minute
-  const minutes = String(Math.round(now.getUTCMinutes() / 2) * 2).padStart(2, '0');
-
-  return `${year}-${month}-${day} ${hours}:${minutes}`;
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 };
 
 const getSlackUser = (req, res) => {
